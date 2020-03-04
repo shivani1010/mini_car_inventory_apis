6b6ae46d8c5dced8a978b33540c8fed9ec3f4787
unknown <lib.shivanigosavi@gmail.com>
2020-03-04 23:56:31 +0530

    Initial Setup

---
+++ assets/php/class.user.php
@@ -0,0 +1,117 @@
+<?php
+include "db_config.php";
+
+	class User{
+		
+
+		public $db;
+
+		public function __construct(){
+			$this->db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABSE);
+
+			if(mysqli_connect_errno()) {
+				echo "Error: Could not connect to database.";
+			        exit;
+			}
+		}
+
+
+
+		public function get_model_details($model_id)
+		{
+
+		$stmt = $this->db->prepare("SELECT model_id,model_name,model_color,model_manufacturer_year,table1.manufacturer_id,manufacturer_name FROM model_tb as table1 INNER JOIN manufacturer_tb as table2 ON table1.manufacturer_id = table2.manufacturer_id where  model_id=?");
+        $stmt->bind_param("s", $model_id);
+        $stmt->execute();
+        $stmt->bind_result($model_id,$model_name,$model_color,$model_manufacturer_year,$manufacturer_id,$manufacturer_name);
+        $stmt->fetch();
+		$result = array();
+
+        $result['model_id'] = $model_id;
+        $result['model_name'] = $model_name;
+        $result['model_color'] = $model_color;
+        $result['model_manufacturer_year'] = $model_manufacturer_year;
+		$result['manufacturer_id'] = $manufacturer_id;
+		$result['manufacturer_name'] = $manufacturer_name;
+		return $result;
+			
+		}
+
+		
+		public function get_manufacturer_lists(){
+
+			$sql="SELECT * FROM manufacturer_tb order by manufacturer_id DESC ";	
+        	$result = mysqli_query($this->db,$sql);
+
+        	return $result;
+			
+		}
+
+		public function get_model_lists(){
+
+
+			$sql="SELECT * FROM model_tb as table1 INNER JOIN manufacturer_tb as table2 ON table1.manufacturer_id = table2.manufacturer_id order by model_id DESC";
+
+        	$result = mysqli_query($this->db,$sql);
+        
+        	return $result;
+			
+		}
+
+
+		public function get_row_count($table,$cond){
+
+			$sql2="SELECT COUNT(*) FROM $table where 1 $cond";
+			
+			$result = mysqli_query($this->db,$sql2);
+			$row = mysqli_fetch_row($result);   
+        
+        	return $row[0];
+			
+		}
+
+
+		public function add_new_manufaturer($manufaturer_name)
+		{
+		
+			$sql="INSERT INTO manufacturer_tb (manufacturer_name) VALUES ('$manufaturer_name')";
+			$result = mysqli_query($this->db,$sql);
+			return $result;
+
+		}
+
+		public function add_images($model_id,$image_name)
+		{
+		
+			$sql="INSERT INTO model_images (model_id,image_path) VALUES ('$model_id','$image_name')";
+			$result = mysqli_query($this->db,$sql);
+			return $result;
+
+		}
+
+
+		
+		public function add_new_model($model_id,$model_name, $model_color,$model_manufacturer_year,$manufacturer_id,$count,$registration_number,$note)
+		{
+				
+			$sql="INSERT INTO model_tb (model_id,model_name, model_color,model_manufacturer_year,registration_number,Note,inventory,manufacturer_id)
+			VALUES ('$model_id','$model_name', '$model_color', '$model_manufacturer_year', '$registration_number', '$note', '$count', '$manufacturer_id')";
+			
+        	$result = mysqli_query($this->db,$sql);
+        	
+        	return $result;
+		}
+
+
+		public function delete_model($model_id){
+			
+			$sql="delete from model_tb where model_id=$model_id";
+		
+			$result = mysqli_query($this->db,$sql);
+			
+			return $result;
+		}
+
+	
+	}
+?>
---
+++ public/index.php
@@ -0,0 +1,202 @@
+<?php
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Http\UploadedFile;
+//use Slim\Factory\AppFactory;
+
+require '../vendor/autoload.php';
+require '../assets/php/class.user.php';
+
+//$app = new \Slim\App;
+$app = new \Slim\App(['settings' => ['displayErrorDetails' => true,'addContentLengthHeader' => false]]);
+
+$container = $app->getContainer();
+$container['upload_directory'] = __DIR__ . '/uploads';
+
+$app->get('/model_details/{model_id}', function ($request, $response, $args) {
+
+	$model_id=$args['model_id'];
+	$user = new User();
+
+	$query=$user->get_model_details($model_id);
+	$jsondata=json_encode(array("error"=>false,"model_details" => $query));
+	
+	return $response->write($jsondata);
+	});
+
+
+	$app->get('/manufacturer_lists', function ($request, $response, $args) {
+
+		$user = new User();
+		
+			$query=$user->get_manufacturer_lists();
+			$responseData = array();
+			while($res=mysqli_fetch_array($query))
+			{
+			   $temp = array();
+			   $temp['manufacturer_id'] =  $res['manufacturer_id'];
+			   $temp['manufacturer_name'] =  $res['manufacturer_name'];
+			   array_push($responseData, $temp);
+			  
+			}
+			$jsondata=json_encode($responseData);
+	
+			return $response->write($jsondata);
+		});
+
+
+		$app->get('/model_lists', function ($request, $response, $args) {
+
+			$user = new User();
+			
+				$query=$user->get_model_lists();
+				$responseData = array();
+				while($res=mysqli_fetch_array($query))
+				{
+				   $temp = array();
+				   $temp['model_id'] =  $res['model_id'];
+				   $temp['model_name'] =  $res['model_name'];
+				   $temp['model_color'] =  $res['model_color'];
+				   $temp['model_manufacturer_year'] =  $res['model_manufacturer_year'];
+                   $temp['manufacturer_id'] =  $res['manufacturer_id'];
+				   $temp['manufacturer_name'] =  $res['manufacturer_name'];
+				   $temp['registration_number'] =  $res['registration_number'];
+				   $temp['Note'] =  $res['Note'];
+				   $temp['count'] =  $res['inventory'];
+				   array_push($responseData, $temp);
+				  
+				}
+				$jsondata=json_encode($responseData);
+		
+				return $response->write($jsondata);
+			});
+	
+
+
+
+$app->post('/add_manufacturer', function (Request $request, Response $response) {
+    
+    
+      
+        $requestData = $request->getParsedBody();
+        $manufacturer_name = $requestData['manufacturer_name'];
+       
+		$user = new User();
+        $responseData = array();
+
+        $result = $user->add_new_manufaturer($manufacturer_name);
+
+        if ($result) {
+            $responseData['error'] = false;
+            $responseData['message'] = 'Manufacturer added successfully';
+           
+         } else {
+            $responseData['error'] = true;
+            $responseData['message'] = 'Error in adding manufacturer';
+        }
+
+        $response->getBody()->write(json_encode($responseData));
+   
+});
+
+$app->post('/upload_images', function(Request $request, Response $response) {
+	$requestData = $request->getParsedBody();
+	$model_id = $requestData['model_id'];
+    $directory = $this->get('upload_directory');
+	$user = new User();
+	$responseData = array();
+	$result="";
+    $uploadedFiles = $request->getUploadedFiles();
+	foreach ($uploadedFiles['modelimages'] as $uploadedFile) {
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $filename = moveUploadedFile($directory, $uploadedFile);
+			 $result = $user->add_images($model_id,$filename);
+        }
+    }
+
+    $response->getBody()->write(json_encode($result));
+});
+
+/**
+ * Moves the uploaded file to the upload directory and assigns it a unique name
+ * to avoid overwriting an existing uploaded file.
+ *
+ * @param string $directory directory to which the file is moved
+ * @param UploadedFile $uploadedFile file uploaded file to move
+ * @return string filename of moved file
+ */
+function moveUploadedFile($directory, UploadedFile $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $basename = bin2hex(random_bytes(8)); // see http://php.net/manual/en/function.random-bytes.php
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
+}
+
+$app->post('/add_model', function (Request $request, Response $response) {
+    
+        $requestData = $request->getParsedBody();
+		$model_name = $requestData['model_name'];
+		$model_color = $requestData['model_color'];
+		$model_manufacturer_year = $requestData['model_manufacturer_year'];
+		$manufacturer_id = $requestData['manufacturer_id'];
+		$registration_number = $requestData['registration_number'];
+		$note = $requestData['note'];
+		$count = 1;
+
+		$user = new User();
+        $responseData = array();
+		
+		$model_id = mt_rand(10000, 99999);
+        $result = $user->add_new_model($model_id,$model_name, $model_color,$model_manufacturer_year,$manufacturer_id,$count,$registration_number,$note);
+
+        if ($result) {
+            $responseData['error'] = false;
+			$responseData['message'] = 'Model added successfully';
+			$responseData['model_id'] = $model_id;
+           
+         } else {
+            $responseData['error'] = true;
+            $responseData['message'] = 'Error in adding model';
+        }
+
+        $response->getBody()->write(json_encode($responseData));
+    
+});
+
+$app->post('/delete_model', function (Request $request, Response $response) {
+    
+        $requestData = $request->getParsedBody();
+		$model_id = $requestData['model_id'];
+		
+       
+		$user = new User();
+        $responseData = array();
+        $result = $user->delete_model($model_id);
+
+        if ($result) {
+            $responseData['error'] = false;
+            $responseData['message'] = 'Model sold successfully';
+           
+         } else {
+            $responseData['error'] = true;
+            $responseData['message'] = 'Error in updating data';
+        }
+
+        $response->getBody()->write(json_encode($responseData));
+    
+});
+
+
+// Run application
+$app->run();
+
+
+
+
+?>
